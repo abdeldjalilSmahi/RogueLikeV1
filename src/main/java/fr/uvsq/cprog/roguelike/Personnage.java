79bcebfd9da157104d14a6189f61f5ee54cf4e9a
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 21:28:25 +0100

    reformattage de la classe personnage en respectant, googlecheckstyle et la javadoc

--- src/main/java/fr/uvsq/cprog/roguelike/Personnage.java
+++ src/main/java/fr/uvsq/cprog/roguelike/Personnage.java
@@ -2,19 +2,28 @@ package fr.uvsq.cprog.roguelike;
 
 import org.fusesource.jansi.Ansi.Color;
 
+/**
+ * Classe abstraite représentant un personnage dans le jeu RogueLike.
+ */
 public abstract class Personnage extends WorldObject {
 
   /**
-   * Constructeur de {@code WorldObject}.
+   * Constructeur de {@code Personnage}.
    *
-   * @param x         Coordonnée x de l'objet dans le monde.
-   * @param y         Coordonnée y de l'objet dans le monde.
-   * @param asciiChar Caractère ASCII représentant l'objet dans le monde.
-   * @param color     Couleur de l'objet dans le monde.
+   * @param x         Coordonnée x du personnage dans le monde.
+   * @param y         Coordonnée y du personnage dans le monde.
+   * @param asciiChar Caractère ASCII représentant le personnage dans le monde.
+   * @param color     Couleur du personnage dans le monde.
    */
   public Personnage(int x, int y, String asciiChar, Color color) {
     super(x, y, asciiChar, color);
   }
 
+  /**
+   * Méthode permettant de déterminer si le personnage peut se déplacer vers une nouvelle position.
+   *
+   * @param dx Déplacement sur l'axe x.
+   * @param dy Déplacement sur l'axe y.
+   */
   public abstract void canMoveTo(int dx, int dy);
 }
