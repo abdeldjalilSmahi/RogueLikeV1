0f2640466f4c94c05f1491c9ae95b2b87878305d
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 21:33:54 +0100

    création de la classe player, représentant le joueur, et l'ajout des getters et setters

---
+++ src/main/java/fr/uvsq/cprog/roguelike/Player.java
@@ -0,0 +1,55 @@
+package fr.uvsq.cprog.roguelike;
+
+import org.fusesource.jansi.Ansi.Color;
+
+public class Player extends Personnage{
+
+  private int  health;
+  private Weapon weapon;
+  private boolean alive ;
+
+
+  /**
+   * Constructeur de {@code Personnage}.
+   *
+   * @param x         Coordonnée x du personnage dans le monde.
+   * @param y         Coordonnée y du personnage dans le monde.
+   * @param asciiChar Caractère ASCII représentant le personnage dans le monde.
+   * @param color     Couleur du personnage dans le monde.
+   */
+  public Player(int x, int y, String asciiChar, Color color) {
+    super(x, y, " @ ", Color.CYAN);
+    this.health = 100 ;
+    this.weapon = null ;
+    this.alive = true ;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  public void setHealth(int health) {
+    this.health = health;
+  }
+
+  public Weapon getWeapon() {
+    return weapon;
+  }
+
+  public void setWeapon(Weapon weapon) {
+    this.weapon = weapon;
+  }
+
+  public boolean isAlive() {
+    return alive;
+  }
+
+  public void setAlive(boolean alive) {
+    this.alive = alive;
+  }
+
+  @Override
+  public void canMoveTo(int dx, int dy) {
+
+  }
+}
