4c23c8b891cc42856f3dfde33a85188160ddd7fc
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 20:11:38 +0100

    ajout de googlecheckstyle a la classe worldObject avec la gestion des couleurs

--- src/main/java/fr/uvsq/cprog/roguelike/WorldObject.java
+++ src/main/java/fr/uvsq/cprog/roguelike/WorldObject.java
@@ -1,42 +1,117 @@
 package fr.uvsq.cprog.roguelike;
 
-public class WorldObject {
-    protected int x;
-    protected int y;
-    private String asciiChar;
+import org.fusesource.jansi.Ansi.Color;
 
-    public WorldObject(int x, int y, String asciiChar) {
+/**
+ * Classe représentant un objet du monde dans le jeu RogueLike.
+ */
+public class WorldObject {
+
+    /** Coordonnée x de l'objet dans le monde. */
+    protected int x;
+    /** Coordonnée y de l'objet dans le monde. */
+    protected int y;
+    /** Caractère ASCII représentant l'objet dans le monde. */
+    private String asciiChar;
+    /** Couleur de l'objet dans le monde. */
+    private Color color;
+
+    /**
+     * Constructeur de {@code WorldObject}.
+     *
+     * @param x          Coordonnée x de l'objet dans le monde.
+     * @param y          Coordonnée y de l'objet dans le monde.
+     * @param asciiChar  Caractère ASCII représentant l'objet dans le monde.
+     * @param color      Couleur de l'objet dans le monde.
+     */
+    public WorldObject(int x, int y, String asciiChar, Color color) {
+        this.color = color;
         this.x = x;
         this.y = y;
         this.asciiChar = asciiChar;
     }
 
+    /**
+     * Retourne la coordonnée x de l'objet dans le monde.
+     *
+     * @return Coordonnée x de l'objet dans le monde.
+     */
     public int getX() {
         return x;
     }
+
+    /**
+     * Modifie la coordonnée x de l'objet dans le monde.
+     *
+     * @param x Coordonnée x de l'objet dans le monde.
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Retourne la coordonnée y de l'objet dans le monde.
+     *
+     * @return Coordonnée y de l'objet dans le monde.
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Modifie la coordonnée y de l'objet dans le monde.
+     *
+     * @param y Coordonnée y de l'objet dans le monde.
+     */
     public void setY(int y) {
         this.y = y;
     }
 
+    /**
+     * Retourne le caractère ASCII représentant l'objet dans le monde.
+     *
+     * @return Caractère ASCII représentant l'objet dans le monde.
+     */
     public String getAsciiChar() {
         return asciiChar;
     }
 
+    /**
+     * Modifie le caractère ASCII représentant
+     /**
+     * Modifie le caractère ASCII représentant l'objet dans le monde.
+     *
+     * @param asciiChar Caractère ASCII représentant l'objet dans le monde.
+     */
     public void setAsciiChar(String asciiChar) {
         this.asciiChar = asciiChar;
     }
 
-    @Override
-    public String toString() {
-        return getAsciiChar() ;
+    /**
+     * Retourne la couleur de l'objet dans le monde.
+     *
+     * @return Couleur de l'objet dans le monde.
+     */
+    public Color getColor() {
+        return color;
     }
 
+    /**
+     * Modifie la couleur de l'objet dans le monde.
+     *
+     * @param color Couleur de l'objet dans le monde.
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * Retourne le caractère ASCII représentant l'objet dans le monde.
+     *
+     * @return Caractère ASCII représentant l'objet dans le monde.
+     */
+    @Override
+    public String toString() {
+        return getAsciiChar();
+    }
 }
