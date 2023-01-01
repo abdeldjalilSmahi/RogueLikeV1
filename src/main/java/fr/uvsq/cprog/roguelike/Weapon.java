5ba589ab2ae3c7cffdfe9dc1a866ec9db36419ae
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 20:29:55 +0100

    regler les prblms de checkstyle de la classe Weapon

--- src/main/java/fr/uvsq/cprog/roguelike/Weapon.java
+++ src/main/java/fr/uvsq/cprog/roguelike/Weapon.java
@@ -7,49 +7,53 @@ import org.fusesource.jansi.Ansi.Color;
  */
 public class Weapon extends WorldObject {
 
-    /** Type d'arme. */
-    private TypeWeapon type;
-    /** Dégâts infligés par l'arme. */
-    private int damage;
+  /**
+   * Type d'arme.
+   */
+  private TypeWeapon type;
+  /**
+   * Dégâts infligés par l'arme.
+   */
+  private int damage;
 
-    /**
-     * Constructeur de {@code Weapon}.
-     *
-     * @param x     Coordonnée x de l'arme dans le monde.
-     * @param y     Coordonnée y de l'arme dans le monde.
-     * @param type  Type d'arme.
-     */
-    public Weapon(int x, int y, TypeWeapon type) {
-        super(x, y, type.getAsciiChar(), Color.WHITE);
-        this.type = type;
-        this.damage = type.getDamage();
-    }
+  /**
+   * Constructeur de {@code Weapon}.
+   *
+   * @param x    Coordonnée x de l'arme dans le monde.
+   * @param y    Coordonnée y de l'arme dans le monde.
+   * @param type Type d'arme.
+   */
+  public Weapon(int x, int y, TypeWeapon type) {
+    super(x, y, type.getAsciiChar(), Color.WHITE);
+    this.type = type;
+    this.damage = type.getDamage();
+  }
 
-    /**
-     * Retourne le type d'arme.
-     *
-     * @return Type d'arme.
-     */
-    public TypeWeapon getType() {
-        return type;
-    }
+  /**
+   * Retourne le type d'arme.
+   *
+   * @return Type d'arme.
+   */
+  public TypeWeapon getType() {
+    return type;
+  }
 
-    /**
-     * Modifie le type d'arme.
-     *
-     * @param type Type d'arme.
-     */
-    public void setType(TypeWeapon type) {
-        this.type = type;
-        setAsciiChar(type.getAsciiChar());
-    }
+  /**
+   * Modifie le type d'arme.
+   *
+   * @param type Type d'arme.
+   */
+  public void setType(TypeWeapon type) {
+    this.type = type;
+    setAsciiChar(type.getAsciiChar());
+  }
 
-    /**
-     * Retourne les dégâts infligés par l'arme.
-     *
-     * @return Dégâts infligés par l'arme.
-     */
-    public int getDamage() {
-        return this.damage;
-    }
+  /**
+   * Retourne les dégâts infligés par l'arme.
+   *
+   * @return Dégâts infligés par l'arme.
+   */
+  public int getDamage() {
+    return this.damage;
+  }
 }
