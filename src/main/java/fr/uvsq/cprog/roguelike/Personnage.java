8f85f464713edd269c9b935dd701bd29ec37aece
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 21:23:47 +0100

    création de la classe Personnage herite de WorldObjecté

---
+++ src/main/java/fr/uvsq/cprog/roguelike/Personnage.java
@@ -0,0 +1,18 @@
+package fr.uvsq.cprog.roguelike;
+
+import org.fusesource.jansi.Ansi.Color;
+
+public class Personnage extends WorldObject {
+
+  /**
+   * Constructeur de {@code WorldObject}.
+   *
+   * @param x         Coordonnée x de l'objet dans le monde.
+   * @param y         Coordonnée y de l'objet dans le monde.
+   * @param asciiChar Caractère ASCII représentant l'objet dans le monde.
+   * @param color     Couleur de l'objet dans le monde.
+   */
+  public Personnage(int x, int y, String asciiChar, Color color) {
+    super(x, y, asciiChar, color);
+  }
+}
