c190dc4dd4130842136a4515db35b0ec37478138
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 20:29:04 +0100

    regler les prblms de checkstyle de la classe TypeWeapon

--- src/main/java/fr/uvsq/cprog/roguelike/TypeWeapon.java
+++ src/main/java/fr/uvsq/cprog/roguelike/TypeWeapon.java
@@ -5,44 +5,54 @@ package fr.uvsq.cprog.roguelike;
  */
 public enum TypeWeapon {
 
-    /** Hache, inflige 10 points de dégâts. */
-    Axe(" ? ", 10),
-    /** Batte, inflige 5 points de dégâts. */
-    Bat(" ! ", 5),
-    /** Pistolet, inflige 15 points de dégâts. */
-    Gun(" > ", 15);
+  /**
+   * Hache, inflige 10 points de dégâts.
+   */
+  Axe(" ? ", 10),
+  /**
+   * Batte, inflige 5 points de dégâts.
+   */
+  Bat(" ! ", 5),
+  /**
+   * Pistolet, inflige 15 points de dégâts.
+   */
+  Gun(" > ", 15);
 
-    /** Caractère ASCII représentant l'arme dans le monde. */
-    private String asciiChar;
-    /** Dégâts infligés par l'arme. */
-    private int damage;
+  /**
+   * Caractère ASCII représentant l'arme dans le monde.
+   */
+  private String asciiChar;
+  /**
+   * Dégâts infligés par l'arme.
+   */
+  private int damage;
 
-    /**
-     * Constructeur de {@code TypeWeapon}.
-     *
-     * @param asciiChar Caractère ASCII représentant l'arme dans le monde.
-     * @param damage    Dégâts infligés par l'arme.
-     */
-    TypeWeapon(String asciiChar, int damage) {
-        this.asciiChar = asciiChar;
-        this.damage = damage;
-    }
+  /**
+   * Constructeur de {@code TypeWeapon}.
+   *
+   * @param asciiChar Caractère ASCII représentant l'arme dans le monde.
+   * @param damage    Dégâts infligés par l'arme.
+   */
+  TypeWeapon(String asciiChar, int damage) {
+    this.asciiChar = asciiChar;
+    this.damage = damage;
+  }
 
-    /**
-     * Retourne le caractère ASCII représentant l'arme dans le monde.
-     *
-     * @return Caractère ASCII représentant l'arme dans le monde.
-     */
-    public String getAsciiChar() {
-        return asciiChar;
-    }
+  /**
+   * Retourne le caractère ASCII représentant l'arme dans le monde.
+   *
+   * @return Caractère ASCII représentant l'arme dans le monde.
+   */
+  public String getAsciiChar() {
+    return asciiChar;
+  }
 
-    /**
-     * Retourne les dégâts infligés par l'arme.
-     *
-     * @return Dégâts infligés par l'arme.
-     */
-    public int getDamage() {
-        return damage;
-    }
+  /**
+   * Retourne les dégâts infligés par l'arme.
+   *
+   * @return Dégâts infligés par l'arme.
+   */
+  public int getDamage() {
+    return damage;
+  }
 }
