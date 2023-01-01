a8b522cdcb5ba57b47b1cb8a1e4f88e6da529bb0
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 20:21:44 +0100

    ajout de la javadoc et reformatage du code de la classe WorldComponentsType

--- src/main/java/fr/uvsq/cprog/roguelike/WorldComponentsType.java
+++ src/main/java/fr/uvsq/cprog/roguelike/WorldComponentsType.java
@@ -1,16 +1,35 @@
 package fr.uvsq.cprog.roguelike;
 
+/**
+ * Énumération des différents types de composants du monde dans le jeu RogueLike.
+ */
 public enum WorldComponentsType {
-    OBSTACLE(" # "), SOL(" . "), SORTIE(" 0 ");
 
+    /** Obstacle, représenté par un caractère #. */
+    OBSTACLE(" # "),
+    /** Sol, représenté par un caractère . */
+    SOL(" . "),
+    /** Sortie, représentée par un caractère 0. */
+    SORTIE(" 0 ");
+
+    /** Caractère ASCII représentant le composant dans le monde. */
     private String asciiChar;
 
+    /**
+     * Constructeur de {@code WorldComponentsType}.
+     *
+     * @param asciiChar Caractère ASCII représentant le composant dans le monde.
+     */
     WorldComponentsType(String asciiChar) {
         this.asciiChar = asciiChar;
     }
 
+    /**
+     * Retourne le caractère ASCII représentant le composant dans le monde.
+     *
+     * @return Caractère ASCII représentant le composant dans le monde.
+     */
     public String getAsciiChar() {
         return asciiChar;
     }
-
 }
