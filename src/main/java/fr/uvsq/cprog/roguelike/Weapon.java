cb5d8ff9d2f85b420fdc968809ae8d41db96cac3
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 20:26:16 +0100

    ajout de la javadoc et reformatage du code de la classe Weapon

--- src/main/java/fr/uvsq/cprog/roguelike/Weapon.java
+++ src/main/java/fr/uvsq/cprog/roguelike/Weapon.java
@@ -2,23 +2,54 @@ package fr.uvsq.cprog.roguelike;
 
 import org.fusesource.jansi.Ansi.Color;
 
-public class Weapon extends WorldObject{
-    private TypeWeapon type;
+/**
+ * Classe représentant une arme dans le jeu RogueLike.
+ */
+public class Weapon extends WorldObject {
 
+    /** Type d'arme. */
+    private TypeWeapon type;
+    /** Dégâts infligés par l'arme. */
+    private int damage;
+
+    /**
+     * Constructeur de {@code Weapon}.
+     *
+     * @param x     Coordonnée x de l'arme dans le monde.
+     * @param y     Coordonnée y de l'arme dans le monde.
+     * @param type  Type d'arme.
+     */
     public Weapon(int x, int y, TypeWeapon type) {
         super(x, y, type.getAsciiChar(), Color.WHITE);
         this.type = type;
+        this.damage = type.getDamage();
     }
 
+    /**
+     * Retourne le type d'arme.
+     *
+     * @return Type d'arme.
+     */
     public TypeWeapon getType() {
         return type;
     }
 
+    /**
+     * Modifie le type d'arme.
+     *
+     * @param type Type d'arme.
+     */
     public void setType(TypeWeapon type) {
         this.type = type;
         setAsciiChar(type.getAsciiChar());
     }
-    public int getWeaponDamage(){
-        return this.type.getDamage() ;
+
+    /**
+     * Retourne les dégâts infligés par l'arme.
+     *
+     * @return Dégâts infligés par l'arme.
+     */
+    public int getDamage() {
+        return this.damage;
     }
 }
