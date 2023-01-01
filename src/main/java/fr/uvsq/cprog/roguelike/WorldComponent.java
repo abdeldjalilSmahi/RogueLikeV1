fa2c77c4f527a50800c648d47c205aad88aa99c6
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-01 21:22:01 +0100

    reformatant la classe WorldComponent en respectant les régles de google checkstyle et javadoc

--- src/main/java/fr/uvsq/cprog/roguelike/WorldComponent.java
+++ src/main/java/fr/uvsq/cprog/roguelike/WorldComponent.java
@@ -2,28 +2,43 @@ package fr.uvsq.cprog.roguelike;
 
 import org.fusesource.jansi.Ansi.Color;
 
-public class WorldComponent extends WorldObject{
-
-  private WorldComponentsType type ;
-
+/**
+ * Classe représentant un composant du monde dans le jeu RogueLike.
+ */
+public class WorldComponent extends WorldObject {
 
   /**
-   * Constructeur de {@code WorldObject}.
+   * Type de composant.
+   */
+  private WorldComponentsType type;
+
+  /**
+   * Constructeur de {@code WorldComponent}.
    *
-   * @param x         Coordonnée x de l'objet dans le monde.
-   * @param y         Coordonnée y de l'objet dans le monde.
-   * @param asciiChar Caractère ASCII représentant l'objet dans le monde.
-   * @param color     Couleur de l'objet dans le monde.
+   * @param x     Coordonnée x du composant dans le monde.
+   * @param y     Coordonnée y du composant dans le monde.
+   * @param type  Type de composant.
+   * @param color Couleur du composant dans le monde.
    */
   public WorldComponent(int x, int y, WorldComponentsType type, Color color) {
     super(x, y, type.getAsciiChar(), color);
     this.type = type;
   }
 
+  /**
+   * Retourne le type de composant.
+   *
+   * @return Type de composant.
+   */
   public WorldComponentsType getType() {
     return type;
   }
 
+  /**
+   * Modifie le type de composant.
+   *
+   * @param type Type de composant.
+   */
   public void setType(WorldComponentsType type) {
     this.type = type;
   }
