6fa8fe9d55852f29d032454b86d28680ada45100
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 06:01:02 +0100

    Constructeur de la classe World

--- src/main/java/fr/uvsq/cprog/roguelike/World.java
+++ src/main/java/fr/uvsq/cprog/roguelike/World.java
@@ -3,15 +3,25 @@ package fr.uvsq.cprog.roguelike;
 import java.util.ArrayList;
 
 public class World {
+
   private final int WIDTH = 20;
   private final int HEIGHT = 20;
 
   private ArrayList<Monster> monsters;
-  private  ArrayList<Weapon> weapons;
+  private ArrayList<Weapon> weapons;
 
-  private  ArrayList<WorldComponent> worldComponents;
-  private  WorldObject[][] world;
-  private int level ;
+  private ArrayList<WorldComponent> worldComponents;
+  private WorldObject[][] world;
+  private int level;
 
-  private Player player ;
+  private Player player;
+
+  public World(int level) {
+    this.world = new WorldObject[HEIGHT][WIDTH];
+    monsters = new ArrayList<>();
+    weapons = new ArrayList<>();
+    worldComponents = new ArrayList<>();
+    this.level = level;
+  }
+
 }
