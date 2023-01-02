7f8c52a89ea9f8cd786322ba9a6a8e7e2a9e2932
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 07:30:58 +0100

    création de la classe WorldBuilder

---
+++ src/main/java/fr/uvsq/cprog/roguelike/WorldBuilder.java
@@ -0,0 +1,24 @@
+package fr.uvsq.cprog.roguelike;
+
+
+/**
+ * Classe construisant le {@link World}.
+ *
+ * @author jalil
+ */
+
+public class WorldBuilder {
+
+  private World world;
+
+  private int xBounds;
+  private int yBounds;
+  private int level;
+
+  /**
+   * Constructeur de {@code WorldBuilder}.
+   *
+   * @param level Le niveau du monde à construire.
+   */
+
+}
