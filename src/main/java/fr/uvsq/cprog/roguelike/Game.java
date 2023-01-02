acd82197e24d65e97a9c20d2a421ea1cdc8ee22c
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 07:54:48 +0100

    création du constructeur de la classe Game

--- src/main/java/fr/uvsq/cprog/roguelike/Game.java
+++ src/main/java/fr/uvsq/cprog/roguelike/Game.java
@@ -1,5 +1,36 @@
 package fr.uvsq.cprog.roguelike;
 
+
+/**
+ * Classe principale du jeu qui gère l'exécution de la partie.
+ */
 public class Game {
 
+  private transient Player player;
+  private World world;
+  private int level;
+
+  private boolean isFinished;
+
+
+  /**
+   * Constructeur de la classe Game qui crée un nouveau monde en fonction du niveau choisi.
+   *
+   * @param level le niveau choisi pour la partie
+   */
+  public Game(int level) {
+    this.level = level;
+
+    this.world = new WorldBuilder(level)
+        .addSols()
+        .addWalls()
+        .addRandomWalls()
+        .addMonsters()
+        .addWeapons()
+        .addPlayer()
+        .addOutput()
+        .build();
+    this.player = world.getPlayer();
+    this.isFinished = false;
+  }
 }
