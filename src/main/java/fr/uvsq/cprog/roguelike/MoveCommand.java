9216ed5b4d8121e66d12db89ba1971c78965c08b
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 08:51:11 +0100

    création de la commande move du mouvement du joueur

---
+++ src/main/java/fr/uvsq/cprog/roguelike/MoveCommand.java
@@ -0,0 +1,14 @@
+package fr.uvsq.cprog.roguelike;
+
+public class MoveCommand implements Command {
+
+  private Player player;
+  private int dx;
+  private int dy;
+  private World world;
+
+  @Override
+  public void execute() {
+
+  }
+}
