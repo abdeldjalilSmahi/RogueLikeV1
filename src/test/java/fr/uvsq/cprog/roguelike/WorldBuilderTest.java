39cccf3f2afe9676f8874d5c2595cf220f2b47a6
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 07:48:41 +0100

    ajout des test Unitaires de la classe WorldBuilder

--- src/test/java/fr/uvsq/cprog/roguelike/WorldBuilderTest.java
+++ src/test/java/fr/uvsq/cprog/roguelike/WorldBuilderTest.java
@@ -1,5 +1,70 @@
 package fr.uvsq.cprog.roguelike;
 
-public class WorldBuilderTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WorldBuilderTest {
+
+  private WorldBuilder worldBuilder;
+
+  @BeforeEach
+  void setUp() {
+    worldBuilder = new WorldBuilder(1);
+  }
+
+  @Test
+  void testReset() {
+    worldBuilder.reset(1);
+    assertNotNull(worldBuilder.build());
+    assertEquals(1, worldBuilder.build().getLevel());
+  }
+
+  @Test
+  void testAddWalls() {
+    worldBuilder.addWalls();
+    assertTrue(worldBuilder.build().getWorldComponents().size() >= 4);
+  }
+
+  @Test
+  void testAddSols() {
+
+    worldBuilder.addSols();
+    assertTrue(worldBuilder.build().getWorldComponents().size() >= 19*19);
+  }
+
+  @Test
+  void testAddRandomWalls() {
+    worldBuilder.addSols().addRandomWalls();
+    assertTrue(worldBuilder.build().getWorldComponents().size() >= 4);
+  }
+
+
+
+  @Test
+  void testAddRandomWeapons() {
+    worldBuilder.addSols().addWeapons();
+    assertTrue(worldBuilder.build().getWeapons().size() >= 1);
+  }
+
+  @Test
+  void testAddPlayer() {
+    worldBuilder.addPlayer();
+    assertNotNull(worldBuilder.build().getPlayer());
+  }
+
+  @Test
+  void testBuild() {
+    worldBuilder.addWalls().addSols().addRandomWalls().addMonsters().addWeapons().addPlayer();
+    World world = worldBuilder.build();
+    assertNotNull(world);
+    assertTrue(world.getWorldComponents().size() >= 4);
+    assertTrue(world.getMonsters().size() >= 1);
+    assertTrue(world.getWeapons().size() >= 1);
+    assertNotNull(world.getPlayer());
+  }
 
 }
