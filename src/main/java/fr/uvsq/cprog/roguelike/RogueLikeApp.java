ad993f8e3cd289afabbdb68ba70cc2f400c5e862
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 12:00:25 +0100

    ajout de la méthode loadGame pour charger la partie sauvegardée

--- src/main/java/fr/uvsq/cprog/roguelike/RogueLikeApp.java
+++ src/main/java/fr/uvsq/cprog/roguelike/RogueLikeApp.java
@@ -22,7 +22,13 @@ public class RogueLikeApp {
             System.out.println("Il existe une partie déja sauvegardé, tu veux la continuer ? O/N");
             String response = scanner.nextLine();
             switch (response){
+                case "O":
+                    game1 = loadGame();
+                    System.out.println("Game loaded successfuly");
 
+                    Thread.sleep(1000);
+                    Game.clearConsole();
+                    break;
                 case "N":
                     game1 = new Game(1);
                     System.out.println("Une nouvelle partie vas démarrer");
@@ -52,6 +58,50 @@ public class RogueLikeApp {
                 System.out.println(e.getMessage());
             }
         }
+    }
+    private static Game loadGame(){
+
+        Player player = null ;
+        ArrayList<Monster> monsters = null ;
+        ArrayList<Weapon> weapons = null ;
+        ArrayList<WorldComponent> worldComponents = null ;
+        Game game = null ;
+
+        try {
+            // create Gson instance
+            Gson gson = new Gson();
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get("game.json"));
+            String targetString = IOUtils.toString(reader);
+            game= gson.fromJson(targetString, Game.class);
+
+            // close reader
+            reader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        World world = game.getWorld();
+        WorldObject[][] loadedWorld = new WorldObject[world.getHEIGHT()][world.getWIDTH()];
+        player = world.getPlayer();
+        monsters = world.getMonsters() ;
+        weapons = world.getWeapons();
+        worldComponents = world.getWorldComponents();
+//        game.setWorld(world);
+        loadedWorld[player.getX()][player.getY()] = player ;
+        for(Monster monster : monsters){
+            loadedWorld[monster.getX()][monster.getY()] = monster ;
+        }
+        for(WorldComponent worldComp : worldComponents){
+            loadedWorld[worldComp.getX()][worldComp.getY()]= worldComp ;
+        }
+        for(Weapon weapon : weapons){
+            loadedWorld[weapon.getX()][weapon.getY()]=weapon ;
+        }
+        world.setWorld(loadedWorld);
+        game.setWorld(world);
+        game.setPlayer(world.getPlayer());
+
+        return game ;
     }
 
 }
