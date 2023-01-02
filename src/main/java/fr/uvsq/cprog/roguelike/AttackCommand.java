9d957118b531e6b1f9411fa93e7e0a8d6b9ba84c
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 11:12:27 +0100

    Création de la méthode isMonsterNearby

--- src/main/java/fr/uvsq/cprog/roguelike/AttackCommand.java
+++ src/main/java/fr/uvsq/cprog/roguelike/AttackCommand.java
@@ -1,6 +1,7 @@
 package fr.uvsq.cprog.roguelike;
 
-public class AttackCommand implements Command{
+public class AttackCommand implements Command {
+
   private Player player;
   private World world;
 
@@ -12,5 +13,22 @@ public class AttackCommand implements Command{
   @Override
   public void execute() {
 
+  }
+
+  public boolean isMonsterNearby() {
+    int x = player.getX();
+    int y = player.getY();
+    for (int i = x - 1; i <= x + 1; i++) {
+      for (int j = y - 1; j <= y + 1; j++) {
+        if (i == x && j == y) {
+          continue;
+        }
+        WorldObject object = world.getObject(i, j);
+        if (object instanceof Monster) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
