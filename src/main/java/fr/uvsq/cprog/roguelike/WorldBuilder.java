cf9e602b3505575c39ecca6a2acddb834ca93da0
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 07:36:51 +0100

    création de la méthode reset

--- src/main/java/fr/uvsq/cprog/roguelike/WorldBuilder.java
+++ src/main/java/fr/uvsq/cprog/roguelike/WorldBuilder.java
@@ -20,5 +20,22 @@ public class WorldBuilder {
    *
    * @param level Le niveau du monde à construire.
    */
+  public WorldBuilder(int level) {
+
+    this.level = level;
+    reset(level);
+  }
+
+  /**
+   * Réinitialise le constructeur de monde avec un nouveau niveau.
+   *
+   * @param level Le niveau du monde à construire.
+   */
+  public void reset(int level) {
+    this.world = new World(level);
+    this.xBounds = this.world.getHEIGHT() - 2 - 1;
+    this.yBounds = this.world.getWIDTH() - 2 - 1;
+  }
+
 
 }
