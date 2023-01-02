f7210e66d385bcd2be6dd85ba07ae2fe59bfef38
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 06:01:51 +0100

    getters and setters pour la classe World

--- src/main/java/fr/uvsq/cprog/roguelike/World.java
+++ src/main/java/fr/uvsq/cprog/roguelike/World.java
@@ -24,4 +24,59 @@ public class World {
     this.level = level;
   }
 
+  public int getWIDTH() {
+    return WIDTH;
+  }
+
+  public int getHEIGHT() {
+    return HEIGHT;
+  }
+
+  public ArrayList<Monster> getMonsters() {
+    return monsters;
+  }
+
+  public void setMonsters(ArrayList<Monster> monsters) {
+    this.monsters = monsters;
+  }
+
+  public ArrayList<Weapon> getWeapons() {
+    return weapons;
+  }
+
+  public void setWeapons(ArrayList<Weapon> weapons) {
+    this.weapons = weapons;
+  }
+
+  public ArrayList<WorldComponent> getWorldComponents() {
+    return worldComponents;
+  }
+
+  public void setWorldComponents(ArrayList<WorldComponent> worldComponents) {
+    this.worldComponents = worldComponents;
+  }
+
+  public WorldObject[][] getWorld() {
+    return world;
+  }
+
+  public void setWorld(WorldObject[][] world) {
+    this.world = world;
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
 }
