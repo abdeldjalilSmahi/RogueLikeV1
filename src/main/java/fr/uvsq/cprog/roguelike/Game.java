3dc3d28d27fdc9187b494b2e872d09173a7201f3
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 07:56:00 +0100

    création de la méthode principale du jeu Game

--- src/main/java/fr/uvsq/cprog/roguelike/Game.java
+++ src/main/java/fr/uvsq/cprog/roguelike/Game.java
@@ -1,6 +1,8 @@
 package fr.uvsq.cprog.roguelike;
 
 
+import java.util.Scanner;
+
 /**
  * Classe principale du jeu qui gère l'exécution de la partie.
  */
@@ -32,5 +34,92 @@ public class Game {
         .build();
     this.player = world.getPlayer();
     this.isFinished = false;
+  }
+
+  /**
+   * Méthode principale qui exécute la partie jusqu'à ce que le joueur meurt ou termine le niveau.
+   */
+  public void runGame() {
+
+    Scanner scanner = new Scanner(System.in);
+
+    while ((player.isAlive()) && !(this.isFinished())) {
+      String input = scanner.nextLine();
+      Game.clearConsole();
+      Command command = null;
+      int dx;
+      int dy;
+      switch (input) {
+        case "z":
+          dx = -1;
+          dy = 0;
+          command = new MoveCommand(player, dx, dy, world);
+          break;
+
+        case "s":
+          dx = 1;
+          dy = 0;
+          command = new MoveCommand(player, dx, dy, world);
+          break;
+
+        case "d":
+          dx = 0;
+          dy = 1;
+          command = new MoveCommand(player, dx, dy, world);
+          break;
+
+        case "q":
+          dx = 0;
+          dy = -1;
+          command = new MoveCommand(player, dx, dy, world);
+          break;
+
+        case "e":
+          command = new PickUpWeaponCommand(player, world);
+          break;
+
+        case "a":
+          command = new AttackCommand(player, world);
+          break;
+        case "exit":
+          System.out.println("vous etes au point de quitter la parite, vous voulez sauvegareder la partie ? O/N");
+          input = scanner.nextLine();
+          switch (input) {
+            case "O":
+              command = new SaveCommand(this);
+              break;
+            case "N":
+              System.exit(0);
+          }
+          break;
+
+        default:
+          System.out.println("Commande erronée");
+          displayGame();
+          break;
+      }
+
+      if (command != null) {
+        command.execute();
+        moveMonsters();
+        checkLevelCompleted();
+        displayGame();
+
+      }
+      if (!player.isAlive()) {
+        System.out.println("You have died. Game over.");
+        System.out.println("Voulez-vous rejouer ? O/N");
+        String input1 = scanner.nextLine();
+        if (input1.equals("O")) {
+          // Créer un nouveau jeu avec les mêmes paramètres (level et seed)
+          Game newGame = new Game(this.level);
+          // Rejouer la partie
+          newGame.runGame();
+        } else {
+          // Quitter le jeu
+          break;
+        }
+      }
+    }
   }
 }
