b93f25aa63c4571eb4e19d90bfca16cf11e4bc79
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 08:59:28 +0100

    PickUpWeaponCommand : ajout de methode qui verifie s'il y a une arme autour du joueur

--- src/main/java/fr/uvsq/cprog/roguelike/PickUpWeaponCommand.java
+++ src/main/java/fr/uvsq/cprog/roguelike/PickUpWeaponCommand.java
@@ -14,4 +14,32 @@ public class PickUpWeaponCommand implements Command {
   public void execute() {
 
   }
+  public boolean isWeaponNearby(Player player, World world) {
+    int x = player.getX();
+    int y = player.getY();
+
+    // Vérifier les cases adjacentes
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        int newX = x + i;
+        int newY = y + j;
+        if (newX < 0 || newX >= world.getHEIGHT() || newY < 0 || newY >= world.getWIDTH()) {
+          // Case en dehors du monde, on passe
+          continue;
+        }
+        if(i== 0 && j==0){
+          continue;
+          // c'est la case du joueur
+        }
+        WorldObject object = world.getObject(newX, newY);
+        if (object instanceof Weapon) {
+          // On a trouvé une arme !
+          return true;
+        }
+      }
+    }
+
+    // Aucune arme trouvée
+    return false;
+  }
 }
