959b8d011a2596945ce86a12edf22bb6f7d16bd4
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-01-02 08:45:58 +0100

    création de l'interface Command pour le Command design pettern

---
+++ src/main/java/fr/uvsq/cprog/roguelike/Command.java
@@ -0,0 +1,14 @@
+package fr.uvsq.cprog.roguelike;
+
+/**
+ * Interface permettant de définir un comportement pour les commandes du jeu.
+ */
+public interface Command {
+
+  /**
+   * Méthode exécutant la commande.
+   */
+
+  void execute();
+
+}
